d95e29b688498d8863769d88aaf3235ea22c37e3
Nathan Kern <natek5309@gmail.com>
2014-12-31 12:17:29 -0800

    Change legend labels and position

--- bar_graphs.py
+++ bar_graphs.py
@@ -14,32 +14,40 @@ results = [AN, AH, FN, FH]
 
 data = pd.concat(results)
 
-treatment = data.groupby(by='subject type')
+# Adjust column labels so they will look nice when plotted
+data.columns = pd.Index(
+  ['Subject Type', 'Delta', 'Selected', 'Maximum', 'False Positives', 'False Negatives']
+)
+
+# Get relevant statistics
+treatment = data.groupby(by='Subject Type')
 average = treatment.mean()
 std = treatment.std()
 
 # plot deltas
-average['delta'].plot(kind='bar', yerr=std['delta'], color='#C0C0C0')
+average['Delta'].plot(kind='bar', yerr=std['Delta'], color='#C0C0C0')
 plt.title('Delta by Group')
 plt.xlabel('Age and Treatment Group')
 plt.ylabel('Selected Delta')
 plt.savefig('deltas.tif')
 
 # plot false pos/neg together
-avg_f = average[['false positives', 'false negatives']]
-std_f = std[['false positives', 'false negatives']]
+avg_f = average[['False Positives', 'False Negatives']]
+std_f = std[['False Positives', 'False Negatives']]
 colors_multiple = ['#C0C0C0', '#F0F0F0']
-avg_f.plot(kind='bar', yerr=std_f, color=colors_multiple)
+avg_f.plot(kind='bar', ylim=[0, 90], yerr=std_f, color=colors_multiple)
 plt.title('False Categorization by Group')
 plt.xlabel('Age and Treatment Group')
 plt.ylabel('False Positives')
 plt.savefig('falses.tif')
 
 # plot accuracy and max accuracy together
-avg_a = average[['accuracy', 'max accuracy']]
-std_a = std[['accuracy', 'max accuracy']]
+avges_cols = ['Selected', 'Maximum']
+avg_a = average[avges_cols]
+std_a = std[avges_cols]
 avg_a.plot(kind='bar', yerr=std_a, color=colors_multiple)
-plt.title('Accuracy of Selected Delta VS. Maximum Theoretical Delta')
+plt.title('Accuracy of Selected Delta vs. Maximum Theoretical Accuracy')
+plt.legend(bbox_to_anchor=(1, 0), loc='lower right')
 plt.ylabel('Accuracy')
 plt.xlabel('Age and Treatment Group')
 plt.savefig('accuracy.tif')
